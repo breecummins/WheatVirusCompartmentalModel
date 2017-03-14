4e1384d32174daef170654c4d3a14c277b6b8599
Bree Cummins <breecummins@gmail.com>
2017-03-14 16:49:45 -0600

    I think I fixed it now

--- SImodel.jl
+++ SImodel.jl
@@ -14,12 +14,38 @@ IC_P = [C W VW VWn]
 A = beta.*IC_P
 IC_V = [0, 0, 0.15, 0]
 
-function forwardMap(rows::Array{Int},ic::Array{Float64,1},pow::Int)
+function forwardMap(A::Array{Float64,2},ic::Array{Float64,1},rows::Array{Int,1},pow::Int)
+    # perform forward mapping on the submatrix of indices in rows
+    rows = sort(rows)
     l = length(rows)
-    B = [ A[k,j] for k in rows for j in rows ]
-    B = reshape(B,l,l)
-    B = B.*(ones(l) - ic)
+    ic = [ IC_V[k] for k in rows ]
+    B = reshape([ A[k,j] for k in rows for j in rows ],l,l)'
+    B .*= (ones(l) - ic)
     ( (eye(l)+B)^pow ) * ic
 end
 
-println(forwardMap([1 2 3 4],IC_V,1))
+function test()
+    # const beta = Float64[
+    #                      0.1 0.4 0.4 0.0; 
+    #                      0.5 0.2 0.2 0.2;
+    #                      0.3 0.2 0.05 0.05;
+    #                      0.0 0.2 0.05 0.05;    
+    #                      ]
+    # const C = 100
+    # const W = 200
+    # const VW = 10
+    # const VWn = 15
+    # IC_P = [C W VW VWn]
+    # A = beta.*IC_P
+    # IC_V = [0, 0, 0.15, 0]
+    rows = [1,3,4]
+    println(forwardMap(A,IC_V,rows,1) == [0.6,0.21375,0.075])
+    println(forwardMap(A,IC_V,rows,2))
+    V1 = forwardMap(A,IC_V,rows,1)
+    for (k,r) in enumerate(rows)
+        IC_V[r] = V1[k]
+    end
+    println(forwardMap(A,IC_V,rows,1))
+end
+
+test()
