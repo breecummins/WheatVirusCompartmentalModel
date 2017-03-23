7e0c02adbc7035b8f4119b954c47c6456d389a68
Bree Cummins <breecummins@gmail.com>
2017-03-22 19:15:27 -0600

    map from section 5 in notes is implemented

--- SImodel.jl
+++ SImodel.jl
@@ -1,51 +1,41 @@
-# infection probability: C->C, W->C, C->W, W->W, VW->VW
-# we may need more betas -- could be that just the beta' are equal
-const beta = Float64[
-                     0.1 0.4 0.4 0.0; 
-                     0.5 0.2 0.2 0.2;
-                     0.3 0.2 0.05 0.05;
-                     0.0 0.2 0.05 0.05;    
-                     ]
-const C = 100
-const W = 200
-const VW = 10
-const VWn = 15
-IC_P = [C W VW VWn]
-A = beta.*IC_P
-IC_V = [0, 0, 0.15, 0]
-
-function forwardMap(A::Array{Float64,2},ic::Array{Float64,1},rows::Array{Int,1},pow::Int)
+function forwardMap(A::Array{Float64,2},icv::Array{Float64,1},icp::Array{Int,2},rows::Array{Int,1},pow::Int)
     # perform forward mapping on the submatrix of indices in rows
     rows = sort(rows)
     l = length(rows)
-    ic = [ IC_V[k] for k in rows ]
+    ic = [ icv[k] for k in rows ]
     B = reshape([ A[k,j] for k in rows for j in rows ],l,l)'
     B .*= (ones(l) - ic)
-    ( (eye(l)+B)^pow ) * ic
+    scaling_factor = sum([ icp[k] for k in rows ])
+    ( (eye(l)+B/scaling_factor)^pow ) * ic
 end
 
 function test()
-    # const beta = Float64[
-    #                      0.1 0.4 0.4 0.0; 
-    #                      0.5 0.2 0.2 0.2;
-    #                      0.3 0.2 0.05 0.05;
-    #                      0.0 0.2 0.05 0.05;    
-    #                      ]
-    # const C = 100
-    # const W = 200
-    # const VW = 10
-    # const VWn = 15
-    # IC_P = [C W VW VWn]
-    # A = beta.*IC_P
-    # IC_V = [0, 0, 0.15, 0]
+    # infection probability: C->C, W->C, C->W, W->W, VW->VW
+    # we may need more betas
+    const beta = Float64[
+                         0.1 0.4 0.4 0.0; 
+                         0.5 0.2 0.2 0.2;
+                         0.3 0.2 0.05 0.05;
+                         0.0 0.2 0.05 0.05;    
+                         ]
+    const C = 100
+    const W = 200
+    const VW = 10
+    const VWn = 15
+    IC_P = [C W VW VWn]
+    A = beta.*IC_P
+    IC_V = [0, 0, 0.15, 0]
     rows = [1,3,4]
-    println(forwardMap(A,IC_V,rows,1) == [0.6,0.21375,0.075])
-    println(forwardMap(A,IC_V,rows,2))
-    V1 = forwardMap(A,IC_V,rows,1)
+    println("Power of 1")
+    println(forwardMap(A,IC_V,IC_P,rows,1)) 
+    println("Power of 2 in 1 step")
+    println(forwardMap(A,IC_V,IC_P,rows,2))
+    V1 = forwardMap(A,IC_V,IC_P,rows,1)
     for (k,r) in enumerate(rows)
         IC_V[r] = V1[k]
     end
-    println(forwardMap(A,IC_V,rows,1))
+    println("Power of 2 in 2 steps")
+    println(forwardMap(A,IC_V,IC_P,rows,1))
 end
 
 test()
