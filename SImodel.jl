4efa8df874737001f5ba1b50e83923ff238d8235
Bree Cummins <breecummins@gmail.com>
2017-06-19 15:15:57 -0600

    finishing up multi-year method

--- SImodel.jl
+++ SImodel.jl
@@ -7,7 +7,7 @@ SquareMatrix(X) = SquareMatrix(X,size(X)[1])
 
 type Proportion
     prop::Float64
-    Proportion(prop) = !(0 < prop <1) ? error("not a proportion") : new(prop)
+    Proportion(prop) = !(0 <= prop <= 1) ? error("not a proportion") : new(prop)
 end
 
 type ClimateParameter
@@ -21,6 +21,7 @@ type Parameter
     population::Array{Int}
     beta::SquareMatrix
     climate::ClimateParameter
+    Cnminus1::Int
     tau2::Float64
     tau3::Proportion
     gamma11::Proportion
@@ -51,68 +52,90 @@ function stepwise(param::Parameter,icv::Array{Float64,1},rows::Array{Int,1},pow:
     icv
 end
 
+function tinysteps(param::Parameter,icv::Array{Float64,1},rows::Array{Int,1},pow::Int,numsteps::Int = 20*pow)
+    # limits numerical error
+    timestep = pow/numsteps
+    A = getA(param)
+    while numsteps >0
+        # perform forward mapping of initial conditions icv on populations pop restricted to indices in rows
+        B = A.matrix .* (1 - icv) / sum(param.population[rows])
+        # zero out entries that are not in rows
+        rem = filter(x -> !(x in rows), 1:A.dim) 
+        B[:,rem] = B[rem,:] = 0
+        icv = (eye(A.dim) + timestep*B)*icv
+        numsteps -= 1
+    end
+    icv
+end
+
 function fullyear(param::Parameter,IC_virus = Float64[0, 0.5, 0, 0])
     # volunteer wheat only
     rows0 = [2]
-    x0 = param.time_periods[1]
-    V0 = stepwise(param,IC_virus,rows0,x0)
+    x0 = param.climate.time_periods[1]
+    V0 = tinysteps(param,IC_virus,rows0,x0)
     param.beta.matrix[2,2] = 0.0 # volunteer wheat no longer directly transmits to itself
     # volunteer wheat -> wheat and cheatgrass
     # this is split into 3 different steps to record differing yield loss to infection
     rows1 = [1,2,3]
-    x1 = param.time_periods[2]
-    V1 = stepwise(param,V0,rows1,x1)
+    x1 = param.climate.time_periods[2]
+    V1 = tinysteps(param,V0,rows1,x1)
     # growth period after hibernation
-    x3 = param.time_periods[3]
-    V3 = stepwise(param,V1,rows1,x3)
+    x3 = param.climate.time_periods[3]
+    V3 = tinysteps(param,V1,rows1,x3)
     # plant growth ends
-    x4 = param.time_periods[4]
-    V4 = stepwise(param,V3,rows1,x4)
+    x4 = param.climate.time_periods[4]
+    V4 = tinysteps(param,V3,rows1,x4)
     # wheat -> new volunteer wheat
     rows5 = [3,4]
-    x5 = param.time_periods[5]
-    V5 = forwardMap(param,V4,rows5,x5)
+    x5 = param.climate.time_periods[5]
+    V5 = tinysteps(param,V4,rows5,x5)
     (V1,V3,V5)
 end
 
 function multiyear(num_years::Int,param::Parameter,IC_virus = Float64[0, 0.5, 0, 0])
     results = []
     while num_years > 0
+        println(IC_virus)
         (V1,V3,V5) = fullyear(param,IC_virus)
-        (Cnplus1,VWnplus1,Ynplus1) = stats(param,V1,V3,V5)
-        append!(results,[(Cnplus1,VWnplus1,Ynplus1)])
-        IC_virus = Float64[0, VWnplus1, 0, 0]
+        println((V1,V3,V5))
+        (Cnplus1,Ynplus1) = stats(param,V1,V3,V5)
+        append!(results,[(Cnplus1,param.climate.VWpop,Ynplus1)])
+        IC_virus = Float64[0, V5[4], 0, 0]
+        param.Cnminus1 = param.population[1]
         param.population[1] = Cnplus1
+        param.population[2] = param.climate.VWpop
         num_years -= 1
     end
     results
 end
 
 function stats(param::Parameter,V1::Array{Float64,1},V3::Array{Float64,1},V5::Array{Float64,1})
-    println(V5[3]) # percentage infected wheat
-    Cnminus1 = 0.9*param.population[1] #fake initial condition -- need this value from 2 years back
-    VWnplus1 = V5[4]
-    Cnplus1 = convert(Int,round(0.8*( 1.1*param.population[1] + 0.2*Cnminus1 ))) # 3 fake params
-    Ynplus1 = 0.85*( 0.25*V1[3] + 0.5*(V3[3] - V1[3]) + 1 - V3[3] ) # 3 fake params
-    (Cnplus1,VWnplus1,Ynplus1)
+    VWnplus1 = param.climate.VWpop # constant for now -- will need to add climate functions later
+    Cnplus1 = convert(Int,round(param.tau3.prop*( param.climate.tau1*param.population[1] + param.tau2*param.Cnminus1 ))) 
+    Ynplus1 = param.climate.gamma2.prop*( param.gamma11.prop*V1[3] + param.gamma13.prop*(V3[3] - V1[3]) + 1 - V3[3] ) 
+    (Cnplus1,Ynplus1)
 end
 
-function setparams(climate,ICs=Int[Cnminus1,Cn,VWpop])
-    C = 1000
-    VWpop = 100
-    W = convert(Int,2e7)
-    VWnpop = 150
-    pop = Int[C, VWpop, W, VWnpop]
+function setparams(climate::Function,ICs::Array{Int,1})
+    # ICs=Int[Cnminus1,Cn,VWpop]
+    W = convert(Int,2e7) # plants in one hectare field
+    Cnminus1 = ICs[1]
+    climate_parameter = climate(W)
+    pop = Int[ICs[2], ICs[3], W, climate_parameter.VWpop]
     beta = SquareMatrix(Float64[
                          0.1 0.8 0.8 0.0; 
                          0.5 0.1 0.6 0.0;
                          0.5 0.6 0.6 0.0;
                          0.0 0.0 0.6 0.0;    
                          ])
-    Parameter(pop,beta,time_periods)
+    tau2 = 0.4 #can exceed 1
+    tau3 = Proportion(0.9)
+    gamma11 = Proportion(0.25)
+    gamma13 = Proportion(0.5)
+    Parameter(pop,beta,climate_parameter,Cnminus1,tau2,tau3,gamma11,gamma13)
 end
 
-function climate_ambient(W)
+function climate_ambient_nohail(W)
     VWpop = convert(Int,round(0.05*W))
     x0 = 2 # 2 time units = 4 weeks
     x1 = 3 
@@ -121,13 +144,25 @@ function climate_ambient(W)
     x5 = 1 
     time_periods = Int[x0,x1,x3,x4,x5]
     tau1 = 1.1
-    gamma2 = 0.96 #will be multiplied by C
-    (VWpop, time_periods, tau1, gamma2)
+    gamma2 = Proportion(0.96)
+    ClimateParameter(VWpop, time_periods, tau1, gamma2)
+end
+
+function climate_ambient_hail(W)
+    VWpop = convert(Int,round(0.2*W))
+    x0 = 2 # 2 time units = 4 weeks
+    x1 = 3 
+    x3 = 4 
+    x4 = 2 
+    x5 = 1 
+    time_periods = Int[x0,x1,x3,x4,x5]
+    tau1 = 1.1
+    gamma2 = Proportion(0.96)
+    ClimateParameter(VWpop, time_periods, tau1, gamma2)
 end
 
 
 
-
-param = setparams()
+param = setparams(climate_ambient_nohail,Int[convert(Int,1e6),convert(Int,2.5e6),convert(Int,1e5)])
 println(stats(param,fullyear(param)...))
 println(multiyear(2,param))
