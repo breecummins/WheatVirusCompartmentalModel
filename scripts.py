12162b33e86df2dbbea3f91e7aa2dda605f9ef4c
Bree Cummins <breecummins@gmail.com>
2019-01-02 15:36:53 -0700

    deleted julia script and changed picture construction.

--- scripts.py
+++ scripts.py
@@ -3,15 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-initial_condition = 0.50
+initial_condition = 1.0
 numsteps = 1000
 
 Cvals = list(range(5,45,5))
 WVvals = list(range(10,101,10))
 Wvals = list(range(250,251,20))
-
-# X,Y,Z = np.meshgrid(Cvals,WVvals,Wvals)
-# (a,b,c) = X.shape
 
 param_grid = np.zeros((3, len(Cvals), len(WVvals), len(Wvals)))
 
@@ -29,45 +26,50 @@ zlim=[0.7,1.28]
 
 # print(X.transpose())
 # print(Y.transpose())
-Z = np.squeeze(param_grid[:1,:,:,:1])
+Z1 = np.squeeze(param_grid[:1,:,:,:1])
 # print(Z)
-ax.plot_surface(X.transpose(), Y.transpose(), Z)
-ax.set_xlabel("cheatgrass plants per m^2")
-ax.set_ylabel("volunter wheat per m^2")
-ax.set_zlabel("wheat yield")
-ax.set_zlim(zlim)
-plt.title("ambient")
-plt.savefig("grid_results_IC50_ambient.pdf")
+ax.plot_surface(X.transpose(), Y.transpose(), Z1,label="ambient",color="darksalmon")
+p1 = plt.Rectangle((0, 0), 1, 1, fc="darksalmon")
+# ax.set_xlabel("cheatgrass plants per m^2")
+# ax.set_ylabel("volunter wheat per m^2")
+# ax.set_zlabel("wheat yield")
+# ax.set_zlim(zlim)
+# plt.title("ambient")
+# plt.savefig("grid_results_IC50_ambient.pdf")
 # plt.show()
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
 
 # print(X.transpose())
 # print(Y.transpose())
-Z = np.squeeze(param_grid[1:2,:,:,:1])
+Z2 = np.squeeze(param_grid[1:2,:,:,:1])
 # print(Z)
-ax.plot_surface(X.transpose(), Y.transpose(), Z)
-ax.set_xlabel("cheatgrass plants per m^2")
-ax.set_ylabel("volunter wheat per m^2")
-ax.set_zlabel("wheat yield")
-ax.set_zlim(zlim)
-plt.title("hot")
-plt.savefig("grid_results_IC50_hot.pdf")
+ax.plot_surface(X.transpose(), Y.transpose(), Z2,label="hot",color="red")
+p2 = plt.Rectangle((0, 0), 1, 1, fc="red")
+# ax.set_xlabel("cheatgrass plants per m^2")
+# ax.set_ylabel("volunter wheat per m^2")
+# ax.set_zlabel("wheat yield")
+# ax.set_zlim(zlim)
+# plt.title("hot")
+# plt.savefig("grid_results_IC50_hot.pdf")
 # plt.show()
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
 
 # print(X.transpose())
 # print(Y.transpose())
-Z = np.squeeze(param_grid[2:,:,:,:1])
-# print(Z)
-ax.plot_surface(X.transpose(), Y.transpose(), Z)
+Z3 = np.squeeze(param_grid[2:,:,:,:1])
+ax.plot_surface(X.transpose(), Y.transpose(), Z3,label="hot/dry",color="firebrick")
+p3 = plt.Rectangle((0, 0), 1, 1, fc="firebrick")
 ax.set_xlabel("cheatgrass plants per m^2")
 ax.set_ylabel("volunter wheat per m^2")
 ax.set_zlabel("wheat yield")
 ax.set_zlim(zlim)
-plt.title("hot/dry")
-plt.savefig("grid_results_IC50_hotdry.pdf")
-# plt.show()
+# plt.title("hot/dry")
+# plt.savefig("grid_results_IC50_hotdry.pdf")
+
+ax.legend([p1,p2,p3],['ambient','hot','hot/dry'])
+plt.savefig("grid_results_IC_1.pdf")
+plt.show()
